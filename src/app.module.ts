70be2daa6663dc426a63b78344af34e8696f30af
Alexsjka <alejandromino903@gmail.com>
2026-09-08 20:10:02 -0500

    base de datos up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+<<<<<<< Updated upstream
+=======
+import { createObserveModule } from '@nestjs/observe';
+import { DatabaseModule } from './database/database.module';
+>>>>>>> Stashed changes
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+<<<<<<< Updated upstream
     ConfigModule.forRoot({
       isGlobal: true,
+=======
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    BenefitsModule,
+    ContractsModule,
+    DepartmentsModule,
+    EmployeesModule,
+    PositionsModule,
+    ObserveModule.forRoot({
+      appKey: 'YOUR_APP_KEY',
+      appSecret: 'YOUR_APP_SECRET',
+      serviceId: 'human-core-solutions',
+>>>>>>> Stashed changes
     }),
 
     DatabaseModule,
